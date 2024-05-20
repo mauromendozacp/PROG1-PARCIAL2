ad508f0fd3dffaef5ce8f2419dc1018757b72355
Mauro <mauro.mendozacp@gmail.com>
2024-05-20 19:34:55 -0300

    Add initial files for the game

---
+++ Assets/Game/System/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public enum SceneGame
+{
+    Mainmenu,
+    Gameplay
+}
+
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+    public void ChangeScene(SceneGame scene)
+    {
+        string sceneName;
+
+        switch (scene)
+        {
+            case SceneGame.Mainmenu:
+                sceneName = "MainMenu";
+                break;
+            case SceneGame.Gameplay:
+                sceneName = "Gameplay";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(scene), scene, null);
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Game/System/Scripts/Utils.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    public static bool CheckLayerInMask(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}

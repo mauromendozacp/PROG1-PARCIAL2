c455cc85ff4622e0c230ec237bbd7f0cd249e996
Mauro <mauro.mendozacp@gmail.com>
2024-05-21 21:51:04 -0300

    Implement rotate player at mouse position

--- Assets/Game/Gameplay/Shooter/Player/Scripts/PlayerController.cs
+++ Assets/Game/Gameplay/Shooter/Player/Scripts/PlayerController.cs
@@ -3,12 +3,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform body = null;
     [SerializeField] private Animator animator = null;
+    [SerializeField] private LayerMask groundLayer = default;
     [SerializeField] private float speed = 0f;
     [SerializeField] private float mass = 1f;
 
     private CharacterController characterController = null;
-
+    private Camera mainCamera = null;
     private Vector2 move = Vector2.zero;
     private float fallVelocity = 0f;
 
@@ -17,12 +19,15 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
     }
 
     private void Update()
     {
         ApplyGravity();
         Movement();
+        LookAtMouse();
+
         UpdateAnimations();
     }
 
@@ -48,6 +53,17 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(move.x, fallVelocity, move.y);
 
         characterController.Move(movement * Time.deltaTime * speed);
+    }
+
+    private void LookAtMouse()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundLayer))
+        {
+            Vector3 direction = hit.point - transform.position;
+            direction.y = 0f;
+            body.forward = direction;
+        }
     }
 
     private void UpdateAnimations()

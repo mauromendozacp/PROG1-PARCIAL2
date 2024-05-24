19da51ae1f0888b5b3ad9ab2de816c3c2d78e780
Mauro <mauro.mendozacp@gmail.com>
2024-05-23 21:16:33 -0300

    Update gameplay and menu scenes

---
+++ Assets/Game/Gameplay/Shooter/Scripts/GameplayController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    [Header("Reference Settings"), Space]
+    [SerializeField] private PlayerController playerController = null;
+    [SerializeField] private GameplayUI gameplayUI = null;
+}

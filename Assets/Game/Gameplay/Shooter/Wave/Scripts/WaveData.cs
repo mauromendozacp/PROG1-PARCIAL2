132ba06af45911b68a0c608f04414c48a782ba67
Mauro Mendoza <mauro.mendozacp@gmail.com>
2024-06-15 12:47:58 -0300

    Add start wave delay

--- Assets/Game/Gameplay/Shooter/Wave/Scripts/WaveData.cs
+++ Assets/Game/Gameplay/Shooter/Wave/Scripts/WaveData.cs
@@ -6,8 +6,10 @@ public class WaveData : ScriptableObject
     [SerializeField] private ENEMY_TYPE[] availableEnemies = null;
     [SerializeField] private int enemyCount = 0;
     [SerializeField] private float spawnDelay = 0f;
+    [SerializeField] private float startWaveDelay = 0f;
 
     public ENEMY_TYPE[] AvailableEnemies { get => availableEnemies; }
     public int EnemyCount { get => enemyCount; }
     public float SpawnDelay { get => spawnDelay; }
+    public float StartWaveDelay { get => startWaveDelay; }
 }

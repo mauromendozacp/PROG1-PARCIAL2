d1ebaad1ac59ee01ee8ad5fbc9d611ebcb728f25
Mauro Mendoza <mauro.mendozacp@gmail.com>
2024-06-16 23:14:30 -0300

    Implement spawn slimes in win animation

--- Assets/Game/Gameplay/Shooter/Scripts/WinAnimationController.cs
+++ Assets/Game/Gameplay/Shooter/Scripts/WinAnimationController.cs
@@ -7,11 +7,15 @@ using Cinemachine;
 
 public class WinAnimationController : MonoBehaviour
 {
-    [SerializeField] private GameObject chibiPrefab = null;
     [SerializeField] private float focusRuneTransitionTime = 0f;
     [SerializeField] private float focusRuneExtraTime = 0f;
     [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
     [SerializeField] private Vector3 offset = Vector3.zero;
+
+    [SerializeField] private float radius = 0f;
+    [SerializeField] private int spawnCount = 0;
+    [SerializeField] private float spawnDelay = 0f;
+    [SerializeField] private GameObject[] chibiPrefabs = null;
 
     public void PlayWinAnimation(Vector3 focusPosition, Action onFinish)
     {
@@ -32,6 +36,23 @@ public class WinAnimationController : MonoBehaviour
                 virtualCamera.transform.position = Vector3.Lerp(startPosition, targetPosition, timer / focusRuneTransitionTime);
 
                 yield return new WaitForEndOfFrame();
+            }
+
+            for (int i = 0; i < spawnCount; i++)
+            {
+                float angle = i * Mathf.PI * 2 / spawnCount;
+
+                float x = Mathf.Cos(angle) * radius;
+                float z = Mathf.Sin(angle) * radius;
+
+                Vector3 spawnPosition = new Vector3(focusPosition.x + x, focusPosition.y, focusPosition.z + z);
+
+                int randomIndex = UnityEngine.Random.Range(0, chibiPrefabs.Length);
+                GameObject chibiGO = Instantiate(chibiPrefabs[randomIndex], transform);
+                chibiGO.transform.position = spawnPosition;
+                chibiGO.transform.forward = Vector3.back;
+
+                yield return new WaitForSeconds(spawnDelay);
             }
 
             yield return new WaitForSeconds(focusRuneExtraTime);
